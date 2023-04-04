b70f2c0083523eeae0a09da976b7a758add2fa5a
dxvnee <dxvnee>
2023-04-04 16:00:50 +0700

    Modul07

---
+++ PBW lagi/nomor1.php
@@ -0,0 +1,10 @@
+<?php
+    //STRING FUNCTION
+    $string = "D3 Rekayasa Perangkat Lunak Aplikasi";
+
+    //display 'Rekayasa Perangkat Lunak Aplikasi'
+    echo"".substr($string, 3);
+    echo"\n".substr($string, -8);
+    echo"\n".substr($string, 3, 24);
+
+?> 
